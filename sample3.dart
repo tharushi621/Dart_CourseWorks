dd2d97c83536a1c4ece4224628a89f9e59c3ac34
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-03-24 21:25:00 +0530

    create sample3.dart

---
+++ sample3.dart
@@ -0,0 +1,39 @@
+void main(){
+//lists - duplicates are allowed
+  List<String> cartItems=["Laptop","Mouse","Keyboard","Mouse"];
+  print(cartItems);
+  print(cartItems[0]);
+  print(cartItems[1]);
+  cartItems[1]="Wireless Mouse";
+  print(cartItems);
+
+cartItems.add("USB Cable");
+cartItems.remove("Keyboard");
+print(cartItems);
+
+print(cartItems.join(", "));
+
+for(var items in cartItems){
+  print(items);
+}
+
+print(cartItems.length);
+print(cartItems.contains("Laptop"));
+print(cartItems.isEmpty);
+
+//sets - no duplicates
+Set<String> emails={"alex@gmail.com","sam@gmail.com"};
+print(emails);
+print(emails.join(", "));
+for(var email in emails){
+  print(email);
+}
+emails.add("lina@gamil.com");
+emails.add("alex@gmail.com");
+emails.remove("alex@gmail.com");
+emails.add("alex@gmail.com");
+print(emails);
+print(emails.length);
+print(emails.contains("alex@gmail.com"));
+print(emails.isEmpty);
+}
