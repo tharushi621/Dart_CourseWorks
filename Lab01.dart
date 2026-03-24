c39ce72f19ad05b8ef4f7af0cf167049023b8fa3
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-03-24 20:01:03 +0530

    coded

--- Lab01.dart
+++ Lab01.dart
@@ -11,5 +11,33 @@ String myName="Tharu";
 int age=24;
 String city="Kalutara";
 
+print("My name is $myName and I am $age years old from $city");
+print(age+5);
+print("Age after 5 years: "+(age+5).toString());
+
+//task3
+int totalSeconds=125;
+
+print(totalSeconds~/60);
+print(totalSeconds%60);
+
+//task4
+int myAge=24;
+bool hasID=true;
+
+if(myAge>=18 && hasID){
+  print("Access granted");
+}else if (myAge>=18 || !hasID){
+  print("ID required");
+}else{
+  print("Aceess denied");
+}
+
+//task5
+for (int i=1;i<=5;i++){
+  print(i);
+}
+
+//task6
 
 }
