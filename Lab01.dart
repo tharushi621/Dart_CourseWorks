ad62bf2440ef152d102018944ee0b299de16d454
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-03-24 19:46:58 +0530

    create Lab01.dart

---
+++ Lab01.dart
@@ -0,0 +1,15 @@
+void main(){
+//task1
+  String name="Tharushi";
+
+  print("Welcome $name");
+  print("Prpgram starts");
+  print("Program ends");
+
+//task2
+String myName="Tharu";
+int age=24;
+String city="Kalutara";
+
+
+}
