af276ed44242e78c2c7f3313bd6fef1ac6c1aed9
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-03-24 19:15:50 +0530

    add more concepts

--- sample1.dart
+++ sample1.dart
@@ -3,8 +3,9 @@ void main(){ //entry point
     print("Hi");
     print("Hello Dart!");
     print("My name is Alex.");
-    print("My age is 20"); //Withing the "" numbers also treated as texts
+    print("My age is 20"); //Withing the "" numbers are also treated as texts
 
+//variables
     String name = "Tharu";
     int age = 24;
     double height = 5.4;
@@ -17,8 +18,10 @@ void main(){ //entry point
     print(isStudent);
     print(country);
     print("Name : "+name);
+//convert int age into string
     print("Age is : "+age.toString());
     print("Height is : "+height.toString());
+//String adding
     print("Tharushi"+" Bhagya");
 
     int a = 10;
@@ -26,27 +29,121 @@ void main(){ //entry point
     print(a+b);
 
     print("My name is "+name+", I am "+age.toString()+ " years old and I live in " +country);
+//$
     print("My name is $name, I am $age years old and I live in $country");
 
+//operators
     int c = 10;
     int d = 30;
 
     print(c+d);
     print(c-d);
     print(c*d);
-    print(c/d);
-    print(c~/d);
-    print(c%d);
+    print(c/d);   //decimal division
+    print(c~/d);  //integer division
+    print(c%d);   //remainder
 
-    a +=2;
+    a +=2;   //a=a+2
     print(a);
     
+//conditions check
     int marks = 65;
 
     print(marks>=50);
     print(marks<40);
 
-    bool hasId = true;
+    bool hasID = true;
 
-    print(marks>=50 && hasId);
+    print(marks>=50 && hasID);
+    print(marks>=50 || hasID);
+
+//if - else
+    int myAge = 70;
+    if (myAge>=60){
+      print("Senior citizen");
+     }
+     else if (myAge>=18){
+      print("Allowed");
+     }
+     else{
+      print("Not Allowed");
+     }
+
+//switch
+    String role = "user";
+
+    switch (role){
+      case "admin":
+        print("Full Access");
+        break;
+      case "user":
+        print("Limited access");
+        break;
+      default:
+        print("No Access");
+    }
+
+//loops 
+//for
+for (int i = 0; i<=5; i++){
+  print("Counting $i");
+}
+
+//while
+int i=0;
+while(i<=5){
+  print("Counting numbers $i");
+  i++;
+}
+
+//do while
+int j=0;
+do {
+  print("Number $j");
+  j++;
+}while(j<=5);
+
+//break
+for (int i = 1; i <= 5; i++) {
+  if (i == 3) {
+    break;
+  }
+  print(i);
+}
+//continue
+for (int i = 1; i <= 5; i++) {
+  if (i == 3) {
+    continue;
+  }
+  print(i);
+}
+
+//Looping through a list
+List<String> names=["Tharu","Dil","Dew"];
+for(var name in names){
+  print(name);
+}print(names.length);
+names.forEach((n){
+  print(n.toUpperCase());
+});
+//mini lab work
+String username="Tharushi";
+int userAge=24;
+
+ print("Welcome $username");
+
+if(userAge>=18){
+  print("Status: Adult");
+}else{
+  print("Status: Minor");
+}
+//Modern switch
+String grade="A";
+String result=switch(grade){
+  "A"=>"Excellent",
+  "B"=>"Good",
+  "C"=>"Pass",
+   _ =>"Invalid"
+};
+print(result);
 }
