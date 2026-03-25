532f8d44b56315da09fd73a55071edd7af6b26f0
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-03-25 16:01:33 +0530

    Understood the constructors

--- sample5.dart
+++ sample5.dart
@@ -3,8 +3,32 @@ class Car{
   int speed;
 
   Car(this.color,this.speed);
+
+  void start(){
+    print("Car started");
+  }
+  void showDetails(){
+    print("Color: $color");
+    print("Speed: $speed");
+  }
+  void accelerate(){
+    speed += 10;
+    print("Speed increased to $speed");
+  }
 }
 void main(){
-  Car c1=Car("Blue",50);
-  Car c2=Car("White",0);
+  Car c1 = Car("Blue",50);
+  Car c2 = Car("Red",0);
+
+  print("My car details");
+  c1.start();
+  c1.showDetails();
+  c1.accelerate();
+
+  print("");
+
+  print("Your car details");
+  c2.start();
+  c2.showDetails();
+  c2.accelerate();
 }
